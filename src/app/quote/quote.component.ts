0b558eec1ac632cc357d8954a2f44f0782198a5c
AjedidahMwanzia <ajedidah.mwanzia@student.moringaschool.com>
2022-04-08 23:46:55 +0300

    update the quote style

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,7 +8,7 @@ import {Quote} from "../quote"
 })
 export class QuoteComponent implements OnInit {
 quotes:Quote[]=[
-  new Quote( 1, 'You are never too old to set another goal or to dream a new dream',' Malala Yousafzai',0,0),
+  new Quote( 1, 'You are never too old to set another goal or to dream a new dream',' — Malala Yousafzai',0,0),
   new Quote(2, 'For me, becoming isn’t about arriving somewhere or achieving a certain aim. I see it instead as forward motion, a means of evolving, a way to reach continuously toward a better self. The journey doesn’t end.',' — Michelle Obama', 0,0),
   new Quote(3,'I have learned over the years that when one’s mind is made up, this diminishes fear; knowing what must be done does away with fear.','— Rosa Parks',0,0),
   new Quote(4,'We all have problems. But it’s not what happens to us, [it’s] the choices we make after.','— Elizabeth Smart',0,0)
